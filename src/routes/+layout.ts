e6a99cf5c34509eb77fe072910a8d090c88ef49b
codynhanpham <87593612+codynhanpham@users.noreply.github.com>
2026-04-20 17:18:17 -0500

    re-enable CSR

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,4 +1,7 @@
 export const prerender = true;
 export const ssr = true;
-export const csr = false;
+
+// KEEP THIS ON! Removing CSR will break app's interactivity and cause hydration errors!!!
+export const csr = true;
+
 export const trailingSlash = 'never';
